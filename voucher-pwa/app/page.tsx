76d84d7aa3755190920306ec963beece1880ce7a
Reggie <reginaldvaudin@gmail.com>
2026-06-18 16:08:30 +0200

    fix: fixed the missing files

---
+++ voucher-pwa/app/page.tsx
@@ -0,0 +1,13 @@
+﻿// Page were REGGIE VAUDIN Will make the main page what the user sees first
+"use client";
+
+
+export default function Home() {
+  return (
+    <div style={{padding: 20}}>
+      <h1>Voucher PWA App</h1>
+      <p>Welcome to the system</p>
+      
+    </div>
+  );
+}
